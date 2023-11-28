006fc67be64460d3a01e3e1251e10baa2bed9425
mgarbiya <mohamedgarbiya@hotmail.com>
2023-11-28 19:56:43 +0000

    Initial change of the funtion to generate password options

--- script.js
+++ script.js
@@ -90,7 +90,25 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  var passwordLength = parseInt(prompt("How long do you want your password? Please enter a number between 8 - 128."));
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter a number between 8 and 128")
+    return getPasswordOptions();
+  }
+  if (isNaN(passwordLength)) {
+    alert('Please enter a NUMBER between 8 and 128')
+    return getPasswordOptions();
+  }
 
+// Characters in passwords
+
+  var lowercase = confirm("Would you like lowercase letters in your password?")
+  var uppercase = confirm("Would you like uppercase letters in your password?")
+  var numeric = confirm("Would you like numbers in your password?")
+  var special = confirm("Would you like special characters in your password?")
+
+  if (!lowercase && !uppercase && !numeric && !special) {alert('Please choose at least one character type')
+  return getPasswordOptions();
 }
 
 // Function for getting a random element from an array
